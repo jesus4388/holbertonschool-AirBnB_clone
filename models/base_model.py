2e324fb9f5c04a3010a7c5422f292cdcdf958f87
alereginensi <reginensia@gmail.com>
2022-06-28 16:39:08 -0700

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''BaseModel that defines all common attributes/methods for other classes'''
+
+import uuid
+from datetime import datetime
+import time
+
+
+class BaseModel:
+    '''creating BaseModel'''
+    def __init__(self):
+        new_id = uuid.uuid4()
+        self.id = str(new_id)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''defining __str__'''
+        return (f"[<{__class__.__name__}>] <{self.id}> <{self.__dict__}>")
+
+    def save(self):
+        '''defining save'''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''defining to_dict'''
+        dictio = self.__dict__.copy()
+        dictio["__class__"] = __class__.__name__
+        return(dictio)
