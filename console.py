03dd9637265fbfadfe4e3c78ed9314f77d499b13
alereginensi <reginensia@gmail.com>
2022-07-01 08:34:15 -0700

    create command

--- console.py
+++ console.py
@@ -4,7 +4,7 @@
 
 import cmd
 import string, sys
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     '''defining HBNBCommand'''
@@ -30,6 +30,27 @@ class HBNBCommand(cmd.Cmd):
         '''defining help EOF'''
         print ("syntax: EOF"),
         print ("-- terminates the application")
+
+    def do_create(self, arg):
+        """
+        create class name
+        """
+        if arg == "":
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            x = eval(arg)()
+            x.save()
+            print(x.id)
+
+    def do_show(self, arg):
+        if arg == "":
+            print ("** class name missing **")
+        elif arg != "BaseModel":
+            print ("** class doesn't exist **")
+        else:
+            print (BaseModel.__class__.__name__)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
